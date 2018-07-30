845d4c77f1bcd22103694974b57852e2575cce76
Divam <dfordivam@gmail.com>
2018-07-30 20:46:24 +0900

    Some analyses of function equality

---
+++ src/Main.hs
@@ -0,0 +1,112 @@
+{-# language DataKinds #-}
+{-# language OverloadedStrings #-}
+{-# language ScopedTypeVariables #-}
+{-# language GeneralizedNewtypeDeriving #-}
+
+module Main where
+
+import Control.Lens
+import Language.Wasm
+import Language.Wasm.Structure
+import Language.Wasm.Binary
+import Options.Applicative
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.List (foldl', sortOn)
+import Text.Pretty.Simple
+import Numeric.Natural (Natural)
+
+data InputOpts = InputOpts
+  { wasmFileName :: FilePath
+  }
+
+inputOpts = InputOpts
+  <$> strOption
+    (long "input"
+    <> metavar "WASM_FILE"
+    <> help "Input file in wasm binary format")
+
+main :: IO ()
+main = do
+  opts <- execParser (info (inputOpts <**> helper)
+    (fullDesc))
+
+  wasmFile <- BS.readFile (wasmFileName opts)
+  let stats = getStats <$> wmod
+      wmod = decodeModuleLazy wasmFile
+  let
+    (Right (_,(_,(f:fs)))) =
+      (over (_2 . _2) (take 50)
+       . head . reverse
+       . (sortOn (fst . snd))
+       . Map.toList
+       . funcSizeDistribution) <$> stats
+
+    doExprDiff (Function _ _ b1) (Function _ _ b2) = exprDiff b1 b2
+  pPrint $ zip [1..] $ map (doExprDiff f) fs
+  -- pPrintNoColor $ f
+  -- pPrintNoColor $ take 4 $ drop 22 fs
+
+newtype Count = Count Int
+  deriving (Show, Ord, Eq, Num)
+
+newtype Size = Size Int
+  deriving (Show, Ord, Eq, Num)
+
+data Stats = Stats
+  { funcCount :: Count
+  , funcSizeDistribution :: Map (Size, TypeIndex, LocalsType) (Count, [Function])
+  }
+  deriving (Show)
+
+getStats :: Module -> Stats
+getStats wmod = Stats c dist
+  where
+    c = (Count . length . functions) wmod
+    dist = foldl' getDist Map.empty (functions wmod)
+
+    getDist m f@(Function t l b) = Map.alter g (getExpSize b, t, l)  m
+      where g Nothing = Just (Count 1, [f])
+            g (Just (Count c, fs)) = Just (Count (c + 1), f:fs)
+
+getExpSize :: Expression -> Size
+getExpSize [] = Size 0
+getExpSize (i:is) = e + (getExpSize is)
+  where e = case i of
+              (Block _ b) -> getExpSize b
+              (Loop _ b) -> getExpSize b
+              (If _ t f) -> getExpSize t + getExpSize f
+              _ -> Size 1
+
+
+data Diff
+  = VeryDifferent
+  | MinorDiff [(Instruction Natural, Instruction Natural)]
+  | NoDiff
+  deriving (Show)
+
+-- Input Expression of same size
+exprDiff :: Expression -> Expression -> Diff
+exprDiff f1 f2 = loop f1 f2
+  where
+    loop [] [] = NoDiff
+    loop (i1:is1) (i2:is2) = case (g i1 i2) of
+      NoDiff -> loop is1 is2
+      MinorDiff ds -> case (loop is1 is2) of
+        NoDiff -> MinorDiff ds
+        MinorDiff ds2 -> MinorDiff (ds ++ ds2)
+        VeryDifferent -> VeryDifferent
+      VeryDifferent -> VeryDifferent
+    loop _ _ = error "loop got different lengths?"
+
+    g (i1@(Block w1 b1)) (i2@(Block w2 b2)) =
+      if (w1 == w2)
+        then exprDiff b1 b2
+        else VeryDifferent
+
+    g (i1@(I32Const w1)) (i2@(I32Const w2)) =
+      if (w1 == w2) then NoDiff else MinorDiff [(i1,i2)]
+
+    g i1 i2 =
+      if (i1 == i2) then NoDiff else VeryDifferent
